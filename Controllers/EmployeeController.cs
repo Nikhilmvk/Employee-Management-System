9f72d1bc2ae5fe78b69ec1692905fb7480ea2c6b
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-06 15:51:29 +0530

    Update EmployeeController.cs

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,69 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-using DotNetSqlJenkins.Data;
-using DotNetSqlJenkins.Models;
+using Microsoft.EntityFrameworkCore;
+using SimpleEmployeeApp.Data;
+using SimpleEmployeeApp.Models;
 
-namespace DotNetSqlJenkins.Controllers
+namespace SimpleEmployeeApp.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
         private readonly AppDbContext _context;
-
-        public EmployeeController(AppDbContext context)
-        {
-            _context = context;
-        }
+        public EmployeeController(AppDbContext context) => _context = context;
 
         [HttpGet]
-        public IActionResult GetAll()
-        {
-            return Ok(_context.Employees.ToList());
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var emp = _context.Employees.Find(id);
-            if (emp == null)
-                return NotFound();
-
-            return Ok(emp);
-        }
+        public async Task<IActionResult> Get() => Ok(await _context.Employees.ToListAsync());
 
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public async Task<IActionResult> Post(Employee emp)
         {
-            _context.Employees.Add(employee);
-            _context.SaveChanges();
-            return Ok(employee);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Employee employee)
-        {
-            var emp = _context.Employees.Find(id);
-            if (emp == null)
-                return NotFound();
-
-            emp.Name = employee.Name;
-            emp.Email = employee.Email;
-            emp.Department = employee.Department;
-
-            _context.SaveChanges();
+            _context.Employees.Add(emp);
+            await _context.SaveChangesAsync();
             return Ok(emp);
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var emp = _context.Employees.Find(id);
-            if (emp == null)
-                return NotFound();
-
-            _context.Employees.Remove(emp);
-            _context.SaveChanges();
-            return Ok();
         }
     }
 }
