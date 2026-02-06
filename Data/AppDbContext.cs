347c88baa0f57083c9beaf7a4b51e23011b598e6
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-06 15:51:07 +0530

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,15 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using DotNetSqlJenkins.Models;
+using SimpleEmployeeApp.Models;
 
-namespace DotNetSqlJenkins.Data
+namespace SimpleEmployeeApp.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Employee> Employees { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        public DbSet<Employee> Employees { get; set; } = null!;
     }
 }
